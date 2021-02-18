d93e759808881f47506a6d4cc3a156669f0f2ca6
ethanial1 <camiloeiz00@gmail.com>
2021-02-18 14:55:34 -0600

    Se establecio el tipo de respuesta

--- auth.php
+++ auth.php
@@ -2,14 +2,32 @@
 require_once 'clases/auth.class.php';
 require_once 'clases/respuestas.php';
 
-$auth = new auth;
-$respuestas = new respuestas;
+$_auth = new auth;
+$_respuestas = new respuestas;
 
 switch($_SERVER['REQUEST_METHOD']){
     case "POST":
+        //recibimos datos
         $postBody = file_get_contents("php://input");
-        $datosArray = $auth->logIn($postBody);
 
-        print_r(json_encode($datosArray));
+        //enviamos los datos al manejador
+        $datosArray = $_auth->logIn($postBody);
+
+        //devolvemos una respuesta
+        header('Content-Type: application/json');
+        if(isset($datosArray['result']['error_id'])){
+            $response_code = $datosArray['result']['error_id'];
+            http_response_code($response_code);
+        }else{
+            http_response_code(200);
+        }
+
+        //imprimimos la respuesta
+        echo json_encode($datosArray);
+        break;
+    default:
+        header('Content-Type: application/json');
+        $datosArray = $_respuestas->error_400();
+        echo json_encode($datosArray);
         break;
 }
